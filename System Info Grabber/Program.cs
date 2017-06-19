738c8b1eac94b3470dbad35587293ec76ca65594
illlogictech <illlogicrepairs@gmail.com>
2017-06-19 14:07:15 -0400

    Breaking Git

--- System Info Grabber/Program.cs
+++ System Info Grabber/Program.cs
@@ -8,6 +8,7 @@ namespace SystemInfoGrabber
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Testing Git.");
         }
     }
 }
