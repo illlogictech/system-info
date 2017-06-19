8a37acae097ddab54e907512d9857371deb8652e
Jeremy Berkhemer <illlogicrepairs@gmail.com>
2017-06-19 14:08:45 -0400

    Initial Commit of Project

--- System Info Grabber/Program.cs
+++ System Info Grabber/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace SystemInfoGrabber
 {
@@ -7,8 +8,30 @@ namespace SystemInfoGrabber
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Testing Git.");
+            string itemSelect;
+            int itemNumber;
+            Console.Write("24-7 Intouch Data Grabber\n" +
+                          "1. Grab Network Info\n" +
+                          "2. Grab System Info\n" +
+                          "3. Grab Both!\n");
+            itemSelect = Console.ReadLine();
+            itemNumber = Convert.ToInt32(itemSelect);
+            switch (itemNumber)
+            {
+                case 1:
+                    Console.WriteLine("1 was picked!");
+                    break;
+                case 2:
+                    Console.WriteLine("2 was picked!");
+                    break;
+                case 3:
+                    Console.WriteLine("3 was picked!");
+                    break;
+                default:
+                    Console.WriteLine("Invalid selection made.");
+                    break;
+            }
+            return;
         }
     }
 }
